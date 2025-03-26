bad8a0d769dd29cbec20635a34ada323a1e73b18
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2025-03-26 17:53:09 +0100

    login procedure

--- FS0924_BE_S7_L1/Controllers/AccountController.cs
+++ FS0924_BE_S7_L1/Controllers/AccountController.cs
@@ -1,10 +1,15 @@
-﻿using FS0924_BE_S7_L1.DTOs.Account;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using FS0924_BE_S7_L1.DTOs.Account;
 using FS0924_BE_S7_L1.Models.Auth;
 using FS0924_BE_S7_L1.Settings;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 
 namespace FS0924_BE_S7_L1.Controllers
@@ -26,7 +31,7 @@ namespace FS0924_BE_S7_L1.Controllers
             _roleManager = roleManager;
         }
 
-
+        [Authorize(Roles = "Admin")]
         [HttpPost("/[controller]/register")]
         public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequest)
         {
@@ -61,12 +66,45 @@ namespace FS0924_BE_S7_L1.Controllers
                         message = "Ops qualcosa e' andato storto"
                     });
                 }
-            await _userManager.AddToRoleAsync(user, "Admin");
+            await _userManager.AddToRoleAsync(user, "User");
             return Ok(new
             {
                 message = "Utente Registrato Correttamente"
             });
+        }
 
+        [HttpPost("/[controller]/login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequest)
+        {
+            var user = await _userManager.FindByEmailAsync(loginRequest.Email);
+            if (user == null)
+            {
+                return BadRequest(new
+                {
+                    message = "Ops qualcosa e' andato storto"
+                });
+            }
+            await _signInManager.PasswordSignInAsync(user, loginRequest.Password,true,false);
+            var roles = await _signInManager.UserManager.GetRolesAsync(user);
+            List<Claim> claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Email, user.Email));
+            claims.Add(new Claim(ClaimTypes.Name, $"{user.FirstName} {user.LastName}"));
+            foreach(var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.SecurityKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expiry = DateTime.Now.AddMinutes(_jwtSettings.ExpiresInMinutes);
+
+            var token = new JwtSecurityToken(_jwtSettings.Issuer, _jwtSettings.Audience, claims, expires:expiry, signingCredentials: creds);
+            string tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return Ok(new TokenResponse()
+            {
+                Token = tokenString,
+                Expires = expiry,
+            });
         }
 
 
---
+++ FS0924_BE_S7_L1/DTOs/Account/LoginRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FS0924_BE_S7_L1.DTOs.Account
+{
+    public class LoginRequestDto
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
